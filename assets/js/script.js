dad7af95fee82b1dedac4a6e88a9f229a16a2fe4
Jean Ribeiro <jeanribeiro1905@gmail.com>
2020-08-21 17:53:33 -0300

    Só falta o header

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let day_two = document.getElementById("day_two");
 let day_one = document.getElementById("day_one");
 if (day_one.classList.contains('active')) {
     document.getElementById('second-day').style.display = "none";
-    document.getElementById('first-day').style.display = "block";
+    document.getElementById('first-day').style.display = "flex";
 }
 
 day_two.addEventListener("click", function select() {
@@ -10,7 +10,7 @@ day_two.addEventListener("click", function select() {
     day_one.classList.remove('active');
     if (day_two.classList.contains('active')) {
         document.getElementById('first-day').style.display = "none";
-        document.getElementById('second-day').style.display = "block";
+        document.getElementById('second-day').style.display = "flex";
     }
 
 })
@@ -20,7 +20,7 @@ day_one.addEventListener("click", function select() {
     day_two.classList.remove('active');
     if (day_one.classList.contains('active')) {
         document.getElementById('second-day').style.display = "none";
-        document.getElementById('first-day').style.display = "block";
+        document.getElementById('first-day').style.display = "flex";
     }
 
 })
